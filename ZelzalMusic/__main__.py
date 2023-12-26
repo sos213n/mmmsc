8b8905b897c59ce0c298ea0171a2a3a8f6428e4b
ELGAZAR_ALWAZER❤ <108340300+Sjshskdhd@users.noreply.github.com>
2023-12-26 19:11:41 +0200

    Update __main__.py

--- ZelzalMusic/__main__.py
+++ ZelzalMusic/__main__.py
@@ -40,26 +40,26 @@ async def init():
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("ZelzalMusic.plugins" + all_module)
-    LOGGER("ميــوزك زدثــون").info("تم تحميل الاضافات ...✓")
+    LOGGER("ميــوزك الـجـزار").info("تم تحميل الاضافات ...✓")
     await userbot.start()
     await Zelzaly.start()
     try:
         await Zelzaly.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("ميــوزك زدثــون").info(
+        LOGGER("ميــوزك الـجـزار").info(
             "خطأ .. قم بفتح المكالمة في مجموعة السجل الخاصه بك\n\nجارِ ايقاف بوت الميوزك . . ."
         )
         exit()
     except:
         pass
     await Zelzaly.decorators()
-    LOGGER("ميــوزك زدثــون").info(
+    LOGGER("ميــوزك الـجـزار").info(
         "\x44\x6f\x6e\x65\x20\x69\x6e\x73\x74\x61\x6c\x6c\x61\x74\x69\x6f\x6e\x20\x5a\x54\x68\x6f\x6e\x20\x4d\x75\x73\x69\x63\x20\xe2\x9c\x93\x0a\x47\x6f\x20\x74\x6f\x20\x79\x6f\x75\x72\x20\x62\x6f\x74\x20\x61\x6e\x64\x20\x73\x65\x6e\x64\x20\x2f\x73\x74\x61\x72\x74\x0a\x43\x68\x61\x6e\x6e\x65\x6c\x20\x3a\x20\x28\x40\x5a\x54\x68\x6f\x6e\x29"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("ميــوزك زدثــون").info("جارِ ايقاف بوت الميوزك . . .")
+    LOGGER("ميــوزك الـجـزار").info("جارِ ايقاف بوت الميوزك . . .")
 
 
 if __name__ == "__main__":
