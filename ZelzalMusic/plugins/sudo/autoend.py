55a6b645fb740a07439cb98440112d94b5480f3b
ELGAZAR_ALWAZER❤ <108340300+Sjshskdhd@users.noreply.github.com>
2024-01-05 21:01:28 +0200

    Update autoend.py

--- ZelzalMusic/plugins/sudo/autoend.py
+++ ZelzalMusic/plugins/sudo/autoend.py
@@ -17,12 +17,12 @@ async def auto_end_stream(_, message: Message):
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
-    if state == "تفعيل":
+    if state == "on":
         await autoend_on()
         await message.reply_text(
             "» تم تفعيل المغادرة التلقائية للمساعد .. بنجاح\n\nالحساب المساعد سوف يقوم بمغادرة الدردشة تلقائياً .. عند عدم وجود اشخاص في المكالمة"
         )
-    elif state == "تعطيل":
+    elif state == "off":
         await autoend_off()
         await message.reply_text("» تم تعطيل وضع المغادرة التلقائية للمساعد .. بنجاح.")
     else:
