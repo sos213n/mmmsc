ffb72df88516bd5443cbff40b26c495d750ca16e
NoName <57182847+ali-now447@users.noreply.github.com>
2024-01-10 19:18:00 +0300

    Update start.py

--- ZelzalMusic/utils/inline/start.py
+++ ZelzalMusic/utils/inline/start.py
@@ -17,7 +17,7 @@ def start_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/A_M_030"),
+            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/mmmsc"),
         ],
     ]
     return buttons
@@ -33,7 +33,7 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="zzzback")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/A_M_030"),
+            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/mmmsc"),
         ],
     ]
     return buttons
