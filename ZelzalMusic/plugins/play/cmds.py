3fc09602115e843bb3bb6f29385cf0b8a9a462da
NoName <57182847+ali-now447@users.noreply.github.com>
2024-01-13 16:18:13 +0300

    Update cmds.py

--- ZelzalMusic/plugins/play/cmds.py
+++ ZelzalMusic/plugins/play/cmds.py
@@ -20,7 +20,7 @@ async def zdatsr(client: Client, message: Message):
     name = usr.first_name
     usrnam = usr.username
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/9df210c5802b98c5e39f3.jpg",
+        photo=f"https://telegra.ph/file/feeef194eac477862bff6.jpg",
         caption=f"""<b>» مرحبـاً بك عـزيـزي </b> {message.from_user.mention} .\n\n<b>» استخـدم الازرار بالاسفـل 𝄞\n» لـ تصفـح اوامـر الميـوزك 🥁</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
