28583ca3f26bd3d333d5229380dab13fde798cbf
NoName <57182847+ali-now447@users.noreply.github.com>
2024-01-26 18:50:49 +0300

    Update stop.py

--- ZelzalMusic/plugins/admins/stop.py
+++ ZelzalMusic/plugins/admins/stop.py
@@ -15,14 +15,15 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    command(["/stop", "اسكت", "انهاء", "ايقاف"]) & filters.group & ~BANNED_USERS
+    command(["/stop", "اسكت", "انهاء", "ايقاف"]) & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1:
+    if " " in message.text:
         return
     await Zelzaly.stop_stream(chat_id)
     await set_loop(chat_id, 0)
+    user_mention = message.from_user.mention if message.from_user else "المشـرف"
     await message.reply_text(
-        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+        _["admin_5"].format(user_mention), reply_markup=close_markup(_)
     )
