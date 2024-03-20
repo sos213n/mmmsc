9904382b4eacb33dc2fc6d9dd979db04552f7d28
dsosi <158520036+dsosi@users.noreply.github.com>
2024-03-20 06:44:00 +0300

    Update Dev.py

---
+++ ZelzalMusic/plugins/play/Dev.py
@@ -0,0 +1,29 @@
+#sos
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from strings.filters import command
+from AarohiX import app
+import config
+
+@app.on_message(
+    command(["المطور", "السورس", "المصنع"])
+)
+async def maker(client: Client, message: Message):
+    await message.reply_photo(
+        photo="https://te.legra.ph/file/08cec0a2a844713e1624a.jpg",
+        caption="~ Team freedom \n~ Dav Source",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "- مطور البوت .", url=f"tg://openmessage?user_id={config.OWNER_ID}"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "- قناة البوت . ", url=config.SUPPORT_CHAT
+                    ),
+                ],
+            ]
+        ),
+    )
